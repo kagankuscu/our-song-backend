b30d90a8f6e7f5dcf5ee4e96734f3383e331deed
kagankuscu <kagan-kuscu@hotmail.com.tr>
2022-10-17 16:40:26 +0300

    middleware for better naming

--- routes/songRoutes.js
+++ routes/songRoutes.js
@@ -9,10 +9,20 @@ const {
   getSongByName,
   getSongsByWhoLike,
 } = require('../controllers/songController');
+const {
+  singerNameCapitalize,
+  songNameCapitalize,
+} = require('../middleware/nameMiddleware');
 const { parseYoutubeVideoUrl } = require('../middleware/parseVideoUrl');
 const router = express.Router();
 
-router.route('/').get(getSongs).post(parseYoutubeVideoUrl, createNewSong);
+router
+  .route('/')
+  .get(getSongs)
+  .post(
+    [parseYoutubeVideoUrl, singerNameCapitalize, songNameCapitalize],
+    createNewSong
+  );
 router
   .route('/songId/:id')
   .get(getSongById)
