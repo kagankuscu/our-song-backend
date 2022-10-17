092de5c6b4b9d9902e7bc597a0b5568053722cbf
kagankuscu <kagan-kuscu@hotmail.com.tr>
2022-10-17 16:00:49 +0300

    adding new song use middleware that parse url

--- routes/songRoutes.js
+++ routes/songRoutes.js
@@ -9,10 +9,15 @@ const {
   getSongByName,
   getSongsByWhoLike,
 } = require('../controllers/songController');
+const { parseYoutubeVideoUrl } = require('../middleware/parseVideoUrl');
 const router = express.Router();
 
-router.route('/').get(getSongs).post(createNewSong);
-router.route('/songId/:id').get(getSongById).put(uptadeSongById).delete(deleteSongById);
+router.route('/').get(getSongs).post(parseYoutubeVideoUrl, createNewSong);
+router
+  .route('/songId/:id')
+  .get(getSongById)
+  .put(uptadeSongById)
+  .delete(deleteSongById);
 router.get('/singerName/:name', getSongsBySingerName);
 router.get('/songName/:name', getSongByName);
 router.get('/whoLike/:name', getSongsByWhoLike);
